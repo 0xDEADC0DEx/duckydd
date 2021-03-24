70893cbf64162b05d46a421bdc7fcca9ad82c43e
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-03-24 10:58:45 +0100

    Added call of test_become_daemon_success

--- test/test.c
+++ test/test.c
@@ -7,12 +7,14 @@
 
 void test_init_signalhandler(void **state);
 void test_become_daemon_fail(void **state);
+void test_become_daemon_success(void **state);
 
 int unit_test_main()
 {
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(test_init_signalhandler),
 		cmocka_unit_test(test_become_daemon_fail),
+		cmocka_unit_test(test_become_daemon_success),
 	};
 	return cmocka_run_group_tests(tests, NULL, NULL);
 }
--- test/test_wrappers.h
+++ test/test_wrappers.h
@@ -8,10 +8,12 @@
 struct sigaction;
 
 int __wrap_sigaction(int signum, const struct sigaction *act, struct sigaction *oldact);
+
 pid_t __wrap_fork(void);
 pid_t __wrap_setsid(void);
-int __wrap_chdir(const char *path);
+
 mode_t __wrap_umask(mode_t mask);
+int __wrap_chdir(const char *path);
 int __wrap_fclose(FILE *fp);
 FILE *__wrap_freopen(const char *path, const char *mode, FILE *stream);
 #endif
